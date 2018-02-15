ffe48426ff9393d53404afe658727395c976300e
Maximilien Rufin <maximilien.rufin@gmail.com>
2018-02-15 16:53:42 +0100

    back is up and running, will do the front tomorrow

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,25 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(cocktail_params)
+    redirect_to(new_cocktail_dose_path(@cocktail.id))
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
